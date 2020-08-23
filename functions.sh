e90c4b13dc43cb7b7b33878aa3d9149a5fff24a2
Scott Smith <smitherz82@gmail.com>
2020-08-23 20:09:51 +0100

    Updated Dockerfile.devtools and added functions.sh

---
+++ functions.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+function php74() {
+    DOCKER_OPTIONS=
+    while getopts "xmn:p:" opt; do
+        case $opt in
+            x) DOCKER_OPTIONS="$DOCKER_OPTIONS -eXDEBUG_ENABLED=true "
+            ;;
+            m) DOCKER_OPTIONS="$DOCKER_OPTIONS -eMONGODB_ENABLED=true "
+            ;;
+            p) DOCKER_OPTIONS="$DOCKER_OPTIONS -p$OPTARG"
+            ;;
+            p) DOCKER_OPTIONS="$DOCKER_OPTIONS --network $OPTARG"
+            ;;
+            \?) echo "Invalid option -$OPTARG" >&2
+            ;;
+        esac
+    done
+
+    docker run \
+        --rm \
+        -it \
+        -vcomposer-cache:/home/.composer \
+        -vnpm-cache:/home/.npm \
+        -v`pwd`:/var/www \
+        `echo $DOCKER_OPTIONS` \
+        -eUID=`id -u` \
+        -eGID=`id -g` \
+        scottsmith/php:7.4-buster-devtools \
+        sh
+}
