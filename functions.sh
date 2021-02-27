4d17a7addf91ece934e33e17fd4425319a330b0c
Scott Smith <smitherz82@gmail.com>
2021-02-27 20:43:55 +0000

    Updated variables and php8

--- functions.sh
+++ functions.sh
@@ -29,3 +29,33 @@ function php74() {
         scottsmith/php:7.4-buster-devtools \
         sh
 }
+
+function php80() {
+    DOCKER_OPTIONS=
+    while getopts "xmn:p:" opt; do
+        case $opt in
+            x) DOCKER_OPTIONS="$DOCKER_OPTIONS -eXDEBUG_ENABLED=true "
+            ;;
+            m) DOCKER_OPTIONS="$DOCKER_OPTIONS -eMONGODB_ENABLED=true "
+            ;;
+            p) DOCKER_OPTIONS="$DOCKER_OPTIONS -p$OPTARG"
+            ;;
+            n) DOCKER_OPTIONS="$DOCKER_OPTIONS --network $OPTARG"
+            ;;
+            \?) echo "Invalid option -$OPTARG" >&2
+            ;;
+        esac
+    done
+
+    docker run \
+        --rm \
+        -it \
+        -vcomposer-cache:/home/.composer \
+        -vnpm-cache:/home/.npm \
+        -v`pwd`:/var/www \
+        `echo $DOCKER_OPTIONS` \
+        -eUID=`id -u` \
+        -eGID=`id -g` \
+        scottsmith/php:8.0-buster-devtools \
+        sh
+}
